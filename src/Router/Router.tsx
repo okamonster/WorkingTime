a752482318346f233a465f4243a852eff5c4cd3f
ocha <okamonster0615@gmail.com>
2022-09-30 16:46:09 +0900

    first commit

---
+++ src/Router/Router.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter,Routes,Route } from "react-router-dom";
+
+export const Router = ()=> {
+    return(
+        <BrowserRouter>
+            <Routes>
+                <Route path={"/"} element={""}/>
+                <Route path={"/"} element={""}/>
+                <Route path={"/"} element={""}/>
+            </Routes>
+        </BrowserRouter>
+    )
+
+}
